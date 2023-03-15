b206eaf33d3a5f6b0863ca43785a6830240a5f6d
zjh-cpu <79302722+zjh-cpu@users.noreply.github.com>
2023-03-15 16:11:30 +1030

    Add files via upload

---
+++ function-1-1.cpp
@@ -0,0 +1,12 @@
+#include <iostream>
+
+using namespace std;
+
+int print_matrix(int array[10][10]){
+  for(int i=0,i,10,i++){
+    for(int j=0,j<10,j++)
+    cout <<array[10][10]<<endl;
+  }
+
+return 1;
+}
---
+++ function-2-2.cpp
@@ -0,0 +1,15 @@
+#include<iostream>
+#include<math.h>
+
+using namespace std;
+int main(int=array[30][30]){
+    for(int i=0,i<30,i++){
+        for (int j=0,j<30,j++)
+        pow(array[30][30])
+        if array[30][30]
+        return 0;
+        else:
+        printf(array[30][30])
+    }
+    return 0;
+}
