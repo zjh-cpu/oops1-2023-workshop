a8538f00ae0bca2def70f4da79cba34c0932b96b
zjh-cpu <79302722+zjh-cpu@users.noreply.github.com>
2023-03-29 08:59:48 +1030

    Add files via upload

---
+++ Musician.cpp
@@ -0,0 +1,11 @@
+#include <string>
+#include "Musician.h"
+
+Musician::Musician() 
+
+
+Musician::Musician(std::string instrument, int experience) ...
+
+std::string Musician::get_instrument()...
+
+int Musician::get_experience() ...
---
+++ Musician.h
@@ -0,0 +1,18 @@
+#ifndef MUSICIAN_H
+#define MUSICIAN_H
+
+#include <string>
+
+class Musician {
+private:
+    std::string ins;
+    int exp;
+
+public:
+    Musician();
+    Musician(std::string instrument, int experience);
+    std::string get_instrument();
+    int get_experience();
+};
+
+#endif
